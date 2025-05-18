e8adc3cb3cf8c302c84a1f82e465cdd47f8b3f7a
yannick <yannick.kraehenbuehl@students.fhnw.ch>
2025-05-18 22:56:05 +0200

    user stories directory erstellt

--- business_logic/hotel_manager.py
+++ business_logic/hotel_manager.py
@@ -6,5 +6,6 @@ class HotelManager:
     def __init__(self):
         self.__hotels_da = data_access.hotel_data_access()
 
+    # Gibt eine Liste aller Hotels in der angegebenen Stadt zurück
     def read_hotels_by_cty(self,city: str) -> List[model.Hotel]:
         return self.__hotels_da.read_hotels_by_city(city)
