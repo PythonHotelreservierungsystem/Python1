62e1ddad3958794f00823a891bde52850bb37763
CescoMeg <cesco.meggio@gmail.com>
2025-05-19 23:10:22 +0200

    Create und show by id in hotel und address gmacht

--- business_logic/hotel_manager.py
+++ business_logic/hotel_manager.py
@@ -22,7 +22,24 @@ class HotelManager:
     def show_hotels(self, hotel_id: int) -> model.Hotel:
         return self.__hotel_da.show_hotel_by_id(hotel_id)
 
+
+    def show_hotel_by_city(self, city: str) -> model.Hotel:
+        return self.__hotel_da.show_hotel_by_id(1)
+
+xp = HotelManager(HotelDataAccess("../../database/hotel_reservation_sample.db"))
+tist = xp.show_hotel_by_city("Basel")
+
+if tist:
+    print(f"Hotel {tist.name} {tist.stars} {tist.address}")
+else:
+    print("Hotel not found")
+
+
 hm = HotelManager(HotelDataAccess("../../database/hotel_reservation_sample.db"))
 tesst = hm.show_hotels(hotel_id=1)
 
-print(tesst)
+
+if tesst:
+    print(f"Hotel '{tesst.name}' (ID: {tesst.hotel_id}) - {tesst.stars} stars, Address ID {tesst.address}")
+##else:
+   ## print("Hotel not found")
