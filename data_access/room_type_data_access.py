31c06b6246a33924bda82ad20eccade3d9fea397
CescoMeg <cesco.meggio@gmail.com>
2025-05-26 00:58:14 +0200

    Create Room Type gmacht

--- data_access/room_type_data_access.py
+++ data_access/room_type_data_access.py
@@ -6,3 +6,27 @@ from data_access.base_data_access import BaseDataAccess
 class RoomTypeDataAccess(BaseDataAccess):
     def __init__(self, db_path: str = None):
         super().__init__(db_path)
+
+    def create_room_type(
+            self,
+            description: str,
+            max_guests: int,
+    ) -> model.RoomType:
+        if description is None:
+            raise ValueError("description cannot be None")
+        if max_guests is None:
+            raise ValueError("max_guests cannot be None")
+
+        sql ="""
+        INSERT INTO Room_Type (Description, Max_Guests)
+        VALUES (?, ?)"""
+
+        params =(description, max_guests)
+
+        last_row_id, row_count = self.execute(sql, params)
+
+        return model.RoomType(
+            room_type_id=last_row_id,
+            description=description,
+            max_guests=max_guests
+        )
