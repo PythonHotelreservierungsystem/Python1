b3a798dcd436ccd9c0b6746610fc0eedd78aa614
CescoMeg <cesco.meggio@gmail.com>
2025-05-26 01:05:37 +0200

    Create Facility gmacht, allwe unnötig aber egal

--- data_access/__init__.py
+++ data_access/__init__.py
@@ -3,14 +3,14 @@ import sqlite3
 
 from data_access.base_data_access import BaseDataAccess
 from data_access.address_data_access import AddressDataAccess
-##from .admin_data_access import AdminDataAccess
-##from .booking_data_access import BookingDataAccess
-##from .facility_data_access import FacilityDataAccess
-##from .guest_data_access import GuestDataAccess
+from .admin_data_access import AdminDataAccess
+from .booking_data_access import BookingDataAccess
+from .facility_data_access import FacilityDataAccess
+from .guest_data_access import GuestDataAccess
 from data_access.hotel_data_access import HotelDataAccess
-##from .invoice_data_access import InvoiceDataAccess
-##from .room_data_access import RoomDataAccess
-##from .room_type_data_access import RoomTypeDataAccess
+from .invoice_data_access import InvoiceDataAccess
+from .room_data_access import RoomDataAccess
+from .room_type_data_access import RoomTypeDataAccess
 
 def date_to_db(d: date) -> str:
     return d.isoformat()
--- data_access/facility_data_access.py
+++ data_access/facility_data_access.py
@@ -6,3 +6,23 @@ from data_access.base_data_access import BaseDataAccess
 class FacilityDataAccess(BaseDataAccess):
     def __init__(self, db_path: str = None):
         super().__init__(db_path)
+
+    def create_facility(
+            self,
+            facility_name: str
+    ) -> model.Facility:
+        if facility_name is None:
+            raise ValueError("facility_name cannot be None")
+
+        sql="""
+        INSERT INTO Facilities (Facility_Name)
+        VALUES (?)"""
+
+        params =(facility_name,)
+
+        last_row_id, row_count = self.execute(sql, params)
+
+        return model.Facility(
+        facility_id=last_row_id,
+        facility_name=facility_name
+    )
