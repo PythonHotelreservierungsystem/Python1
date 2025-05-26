59cad73d0b0918cd96b9776ce68fd58b1dd52c0f
jennylive006 <118679624+jennylive006@users.noreply.github.com>
2025-05-26 11:25:18 +0200

    SQL Abfrage 1.5

--- test/user_stories/user_story_Abfragen.sql
+++ test/user_stories/user_story_Abfragen.sql
@@ -1,5 +1,5 @@
 ##User Story 1.4
-       SELECT Hotel.hotel_id, Hotel.name, COUNT(Room.room_id) AS available_rooms
+SELECT Hotel.hotel_id, Hotel.name, COUNT(Room.room_id) AS available_rooms
 FROM Hotel
 JOIN Room ON Room.hotel_id = Hotel.hotel_id
 LEFT JOIN Booking ON Booking.room_id = Room.room_id
@@ -9,4 +9,41 @@ LEFT JOIN Booking ON Booking.room_id = Room.room_id
     )
 WHERE Booking.booking_id IS NULL
 GROUP BY Hotel.hotel_id, Hotel.name
+
+##User Story 1.5
+SELECT
+    Hotel.hotel_id,
+    Hotel.name,
+    Hotel.stars,
+    Address.address_id,
+    Room.room_id,
+    Room.room_number,
+    Room_Type.type_id,
+    Room_Type.capacity
+FROM
+    Hotel
+JOIN Address
+ON Hotel.address_id = Address.address_id
+
+JOIN Room
+ON Room.hotel_id = Hotel.hotel_id
+
+JOIN Room_Type
+ON Room_Type.type_id = Room_Type.type_id
+
+LEFT JOIN Booking
+ON Booking.room_id = Room.room_id
+    AND NOT (
+        Booking.check_out_date <= ?
+        OR Booking.check_in_date >= ?
+    )
+WHERE
+    Booking.booking_id IS NULL
+    AND Hotel.stars >= ?
+    AND Room_Type.max_guests >= ?
+    AND Address.city = ?
+ORDER BY
+    Hotel.name,
+    Room.room_number
+
 
