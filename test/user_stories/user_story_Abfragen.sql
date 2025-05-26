4a8b32b7fd0f6b9f5ed4c13185e42b073393a67a
jennylive006 <118679624+jennylive006@users.noreply.github.com>
2025-05-26 13:01:09 +0200

    SQL Abfrage 2.0

--- test/user_stories/user_story_Abfragen.sql
+++ test/user_stories/user_story_Abfragen.sql
@@ -1,4 +1,4 @@
-##User Story 1.4
+#User Story 1.4
 SELECT Hotel.hotel_id, Hotel.name, COUNT(Room.room_id) AS available_rooms
 FROM Hotel
 JOIN Room ON Room.hotel_id = Hotel.hotel_id
@@ -44,12 +44,47 @@ WHERE
 
 
 
-##User Story 1.6
+#User Story 1.6
 SELECT Hotel.hotel_id, Hotel.name, Hotel.stars, Address.street, Address.zip_code, Address.city
 FROM Hotel
 
 JOIN
 Address
 ON Hotel.address_id = Address.address_id
+
+#User Story 2
+
+SELECT
+Hotel.hotel_id,
+Hotel.name,
+Hotel.stars,
+Address.address_id,
+Address.street,
+Address.zip_code,
+Address.city,
+Room.room_id,
+Room.price_per_night,
+Room_Type.max_guests,
+Room_Type.description,
+Room_Facilities.facility_id
+
+FROM
+Hotel
+
+JOIN
+Address
+ON Hotel.address_id = Address.address_id
+
+JOIN
+Room_Type
+ON Room_Type.type_id = Room.type_id
+
+JOIN
+Room
+ON Room.hotel_id = Hotel.hotel_id
+
+JOIN
+Room_Facilities
+ON Room_Facilities.room_id = Room.room_id
 
 
