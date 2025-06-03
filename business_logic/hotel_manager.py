e97def604518da0c7401e0ee94d6283e8a86241c
yannick <yannick.kraehenbuehl@students.fhnw.ch>
2025-06-04 00:13:52 +0200

    Business Logic User Story 1.1 angepasst

--- business_logic/hotel_manager.py
+++ business_logic/hotel_manager.py
@@ -1,39 +1,15 @@
 import model
-
-
-
-from data_access import HotelDataAccess
+from data_access.hotel_data_access import HotelDataAccess
 
 
 class HotelManager:
-    def __init__(self, hotel_id):
-        self.__hotel_da = data_access.HotelDataAccess()
+    def __init__(self, db_path: str):
+        self.__hotel_da = HotelDataAccess(db_path)
 
-    def create_hotel(self,
-                     name: str,
-                     stars: int,
-                     address_id: model.Address = None
-                     ) -> model.Hotel:
-        return self.__hotel_da.create_hotel(
-            name=name,
-            stars=stars,
-            address_id=address_id
-        )
 
-    def show_hotels(self, hotel_id: int) -> model.Hotel:
-        return self.__hotel_da.show_hotel_by_id(hotel_id)
-
-    ##user Story 1.1
-    def show_hotel_by_city(self, city: str) -> model.Hotel:
-        return self.__hotel_da.show_hotel_by_id(1)
-
-xp = HotelManager(HotelDataAccess("../../database/hotel_reservation_sample.db"))
-tist = xp.show_hotel_by_city("Basel")
-
-if tist:
-    print(f"Hotel {tist.name} {tist.stars} {tist.address}")
-else:
-    print("Hotel not found")
-
+    ## user Story 1.1
+    def show_hotels_by_city(self, city: str) -> list[model.Hotel]:
+        #Gibt alle Hotels zurück, deren zugehörige Adresse in der angegebenen Stadt liegt
+        return self.__hotel_da.show_hotels_by_city(city)
 
 
