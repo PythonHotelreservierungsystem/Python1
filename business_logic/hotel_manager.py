5fc802e07dd4431a8f39284666ac06a76e35a5ea
yannick <yannick.kraehenbuehl@students.fhnw.ch>
2025-06-05 00:45:06 +0200

    business loggic user story 1.2

--- business_logic/hotel_manager.py
+++ business_logic/hotel_manager.py
@@ -9,11 +9,28 @@ class HotelManager:
 
 
     ## user Story 1.1
-    ## user Story 1.1
     def show_hotels_by_city(self, city: str) -> list[model.Hotel]:
         # Gibt alle Hotels zurück, deren zugehörige Adresse in der angegebenen Stadt liegt
         alle_hotels = self.__hotel_da.read_all_hotel()
         # nun nur die nach der stadt gesuchten zurückgeben
         return [h for h in alle_hotels if h.address.city == city]
+
+
+    ## user Story 1.2
+    def show_hotels_by_city_and_min_stars(self, city: str, min_stars: int) -> list[model.Hotel]:
+        # Alle Hotels aus der Datenbank holen
+        alle_hotels = self.__hotel_da.read_all_hotel()
+
+        # Filtern nach Stadt und Sterne
+        gefiltert = [
+            h for h in alle_hotels
+            if (h.address.city == city and h.stars >= min_stars)
+        ]
+
+        if not gefiltert:
+            print(f"Keine Hotels in '{city}' mit mindestens {min_stars} Sternen gefunden.")
+            return []
+
+        return gefiltert
 
 
