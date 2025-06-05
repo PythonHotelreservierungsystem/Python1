cb6bfd0c9753b0e49cd490aab3eb7e407b04f143
yannick <yannick.kraehenbuehl@students.fhnw.ch>
2025-06-05 12:39:49 +0200

    model room ahpasst

--- model/room.py
+++ model/room.py
@@ -1,20 +1,20 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-                                            ## Phillip fragen
 from model.facility import Facility
 from model.booking import Booking
 from model.room_type import RoomType
+from model import Hotel
 
 ##Room
 class Room:
     #attributes #?facilites als externe Facility Liste führen? #?room_type als string oder integer?
-    def __init__(self, room_id:int, room_no:int, price_per_night:float, room_type:RoomType, hotel:str, facilities: Facility):
+    def __init__(self, room_id:int, room_number:int, price_per_night:float, room_type:RoomType, hotel:Hotel, facilities: list[Facility]):
         if not room_id:
             raise ValueError("room_id ist erforderlich")
         if not isinstance(room_id, int):
             raise ValueError("room_id muss eine Zahl sein")
         self.__room_id = room_id
-        self.room_no = room_no
+        self.room_no = room_number
         self.price_per_night = price_per_night
         self.room_type = room_type 
         self.hotel = hotel
@@ -29,15 +29,15 @@ class Room:
     
     #room_no Getter und Setter
     @property
-    def room_no(self):
-        return self.__room_no
-    @room_no.setter
-    def room_no(self, value):
+    def room_number(self):
+        return self.__room_number
+    @room_number.setter
+    def room_number(self, value):
         if not value:
             raise ValueError("room_no ist erforderlich")
         if not isinstance(value, str):
             raise ValueError("room_no muss eine Zahl sein")
-        self.__room_no = value
+        self.__room_number = value
 
     #price_per_night Getter und Setter
     @property
