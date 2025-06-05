ddcdbc7ff3c4749a4118fffe923cdc047b55984e
yannick <yannick.kraehenbuehl@students.fhnw.ch>
2025-06-05 16:48:54 +0200

    room data access create hotel

--- data_access/room_data_access.py
+++ data_access/room_data_access.py
@@ -21,37 +21,34 @@ class RoomDataAccess(BaseDataAccess):
     # Room erstellen
     def create_room(
             self,
-            room_no: str,
+            room_number: str,
             price_per_night: float,
             room_type: model.RoomType = None,
             hotel_id: Hotel = None,
             facility_id: Facility = None
     ) -> model.Room:
-        if room_no is None:
+        if room_number is None:
             raise ValueError("Room number cannot be None")
         if price_per_night is None:
             raise ValueError("Price per night cannot be None")
         if room_type is None:
             raise ValueError("Room type cannot be None")
+        sql ="""
+        INSERT INTO Room (Room_Number, Price_Per_Night, Type_Id, Hotel_ID)
+        VALUES (?, ?, ?, ?)"""
 
+        params = (room_number, price_per_night, RoomType.room_type_id, Hotel.hotel_id)
 
-    # def read_all_rooms(self,):
-    #     sql ="""
-    #     INSERT INTO Room (Room_Number, Price_Per_Night, Type_Id, Hotel_ID)
-    #     VALUES (?, ?, ?, ?)"""
-    #
-    #     params = (room_number, price_per_night, Room_Type.room_type_id, Hotel.hotel_id)
-    #
-    #     last_row_id, row_count = self.execute(sql, params)
-    #
-    #     return model.Room(
-    #         room_id=last_row_id,
-    #         room_no=room_number,
-    #         room_type=room_type_id,
-    #         price_per_night=price_per_night,
-    #         hotel=hotel_id,
-    #         facilities=facility_id
-    #     )
+        last_row_id, row_count = self.execute(sql, params)
+
+        return model.Room(
+            room_id=last_row_id,
+            room_number=room_number,
+            room_type=room_type_id,
+            price_per_night=price_per_night,
+            hotel=hotel_id,
+            facilities=facility_id
+        )
 
 
 
