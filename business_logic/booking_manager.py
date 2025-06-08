2abcbaeb432f316334e59f09b6e1db2cc504a232
CescoMeg <cesco.meggio@gmail.com>
2025-06-08 20:38:52 +0200

    Create Invoice with Booking User Story 5

--- business_logic/booking_manager.py
+++ business_logic/booking_manager.py
@@ -1,6 +1,7 @@
 import model
 from datetime import date
 from data_access.booking_data_access import BookingDataAccess
+from data_access.invoice_data_access import InvoiceDataAccess
 from data_access.room_data_access import RoomDataAccess
 from business_logic.room_manager import RoomManager
 
@@ -50,4 +51,45 @@ class BookingManager:
         )
         print("Buchung erfolgreich erstellt.")
         return neue_buchung
+    #User Story 5
+    def Create_Invoice_with_Booking(self, guest: model.Guest, room: model.Room, check_in_date: date,
+                                    check_out_date: date, invoice_da: InvoiceDataAccess) -> model.Invoice:
 
+        verfuegbare_zimmer = self.__room_manager.find_available_rooms_by_dates(check_in_date, check_out_date)
+        if not verfuegbare_zimmer:
+            print("Es gibt keine verfügbaren Räume für Ihren gewünschten Zeitraum.")
+            return None
+
+        print("Verfügbare Zimmer:")
+        for zimmer in verfuegbare_zimmer:
+            print(f"Room-ID: {zimmer.room_id} | Typ: {zimmer.type_id} | Preis/Nacht: {zimmer.price_per_night}")
+
+        while True:
+            try:
+                gewuenschte_id = int(input("Geben Sie die Room-ID des gewünschten Raumes an: "))
+                gewuenschtes_zimmer = next((z for z in verfuegbare_zimmer if z.room_id == gewuenschte_id), None)
+                if gewuenschtes_zimmer:
+                    break
+                else:
+                    print("Kein Zimmer mit dieser ID verfügbar. Bitte erneut versuchen.")
+            except ValueError:
+                print("Ungültige Eingabe. Bitte eine gültige Ganzzahl eingeben.")
+
+        total_naechte = (check_out_date - check_in_date).days
+        gesamtpreis = total_naechte * gewuenschtes_zimmer.price_per_night
+
+        neue_buchung = self.__booking_da.create_booking(
+            guest_id=guest,
+            room_id=gewuenschtes_zimmer,
+            check_in_date=check_in_date,
+            check_out_date=check_out_date,
+            is_cancelled=False,
+            total_amount=gesamtpreis
+        )
+        if not neue_buchung:
+            print("Buchung konnte nicht erstellt werden.")
+            return None
+
+        issue_date = check_out_date
+        invoice = invoice_da.create_invoice(issue_date=issue_date, total_amount=gesamtpreis, booking_id=neue_buchung.booking_id)
+        return invoice
